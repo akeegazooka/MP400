a936380a655eb52a2aa121f77249fd5de8d84b5e
akeegazooka <akeegazooka@gmail.com>
2014-09-30 17:33:03 +0800

    Loading file meta data

--- MP400/src/mp400/PPMFile.java
+++ MP400/src/mp400/PPMFile.java
@@ -9,6 +9,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
@@ -17,30 +18,71 @@ import java.util.ArrayList;
  */
 public class PPMFile {
     ArrayList<Integer[]> imageData;
+    String format;
     MP2d dimensions;
     Integer maxValue;
     String fileName;
     
-    public PPMFile(String inFileName)
+    public PPMFile(String inFileName) throws IOException
     {
         setFileName(inFileName);
+        loadFile();
     }
     
     private void setFileName(String inFileName)
     {
+        fileName = inFileName;
+    }
+    
+    private void loadFile() throws IOException
+    {
         BufferedReader inputStream = null;
+        if(! (fileName.equals("") ) )
+        {
         try
         {
-            inputStream = new BufferedReader(new FileReader(inFileName)) ;
+            inputStream = new BufferedReader(new FileReader(fileName)) ;
+            
             String s;
-            while( (s = inputStream.readLine()) != null )
+            boolean metaFull = false;
+            String dataString = "";
+            String[] dataElements;
+            String[] lineResult;
+            do {
+            while( (s = inputStream.readLine()) !=null)
             {
+                    lineResult = s.split("#");
+                    if(! lineResult[0].equals("") )
+                    {
+                        dataString = dataString.concat(" " + lineResult[0]);
+                        dataElements = dataString.split(" ");
+                        if(dataElements.length == 5)
+                        {
+                            metaFull = true;
+                            for(String sElement : dataElements)
+                            {
+                                System.out.println(sElement);
+                            }
+                        }
+                        
+                    }
                 
-            }
+                }
+                
+            }while (!metaFull);
+            
         } 
         catch (IOException x)
         {
             System.out.println(x.getMessage());
+        }
+        finally
+        {
+            if(inputStream != null)
+            {
+                inputStream.close();
+            }
+        }
         }
     }
     
