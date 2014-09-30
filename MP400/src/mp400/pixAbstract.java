c1964548a7abe2e4eed336fa87e4244189e747e6
akeegazooka <akeegazooka@gmail.com>
2014-10-01 00:48:11 +0800

    added pixabstract

---
+++ MP400/src/mp400/pixAbstract.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mp400;
+
+/**
+ *
+ * @author akeegazooka
+ */
+public abstract class pixAbstract {
+    
+    @Override
+    public abstract String toString();
+    public abstract pixAbstract getPixData();
+            
+    
+}
