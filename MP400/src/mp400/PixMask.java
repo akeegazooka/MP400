446c3df91138866a0ec2ee187454f3529d1452ec
akeegazooka <akeegazooka@gmail.com>
2014-10-01 00:49:26 +0800

    added pixMask

---
+++ MP400/src/mp400/PixMask.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mp400;
+import java.awt.image.Kernel;
+import java.util.ArrayList;
+import sun.java2d.loops.MaskBlit;
+/**
+ *
+ * @author akeegazooka
+ */
+public class PixMask
+{
+    MP2d dimensions;
+    Double[][] kernel;
+    
+    /**
+     *
+     * @param inKernel
+     */
+    public PixMask(Double[][] inKernel)
+    {
+        dimensions = new MP2d(inKernel.length, inKernel[0].length);
+        kernel = inKernel;
+    }
+}
