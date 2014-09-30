8f253f58a85da5dd6c54d526071a0b70364d86b7
akeegazooka <akeegazooka@gmail.com>
2014-09-30 23:11:31 +0800

    convolution has begun

--- MP400/src/mp400/PPMConvolve.java
+++ MP400/src/mp400/PPMConvolve.java
@@ -11,5 +11,35 @@ package mp400;
  * @author akeegazooka
  */
 public class PPMConvolve {
+    PixMask mask;
+    PixMask normalMask;
+    PPMFile imageData;
     
+    public PPMFile convolve(PixMask inMask, PPMFile inImage, String operation)
+    {
+        return imageData;
+    }
+    
+    public PixMask normalizeMask(PixMask inMask)
+    {
+        PixMask normalMask = new PixMask(inMask.kernel);
+        double runningTotal = 0;
+        for(int mX = 0;mX<inMask.dimensions.getX();mX++)
+        {
+            for(int mY = 0;mY<inMask.dimensions.getY();mY++)
+            {
+                runningTotal+=inMask.kernel[mX][mY];
+            }
+        }
+       for(int nX=0;nX<inMask.dimensions.getX();nX++)
+       {
+           for(int nY=0;nY<inMask.dimensions.getY();nY++)
+           {
+               normalMask.kernel[nX][nY] = inMask.kernel[nX][nY] / runningTotal;
+           }
+       }
+               
+        
+        return normalMask;
+    }
 }
